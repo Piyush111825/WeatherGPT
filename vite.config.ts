c6ae7955575f964719d93c691c8a54a0b9bf69b1
Piyush111825 <piyushkumar111825@gmail.com>
2026-09-12 14:23:15 +0530

    Change base path in Vite config to '/WeatherGPT/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/WeatherGPT/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
